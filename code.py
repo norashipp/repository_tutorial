90fc7252e4cfb9ee4668666d410ef2faea1a59dc
Nora Shipp <norashipp@uchicago.edu>
2017-04-24 15:06:07 -0700

    added do_something_else

--- code.py
+++ code.py
@@ -2,3 +2,7 @@
 def do_something():
     something = 'I yam what I yam.'
     print(something)# this will make it much easier in future problems to see that something is actually happening
+
+def do_something_else():
+    something = 'Hello'
+    print(something)
